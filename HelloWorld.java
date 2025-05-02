c7c2237ea8f9ff877bd6310899629f8db073778f
Pradip Kumar Senapati <2021wa86079@wilp.bits-pilani.ac.in>
2025-05-02 18:42:09 +0530

    Updated HelloWorld.java - V2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Java - V1");
+        System.out.println("Hello Java – V2");
+        System.out.println("Git versioning demo”);
     }
 }
+
