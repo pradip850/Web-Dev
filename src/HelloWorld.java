ec30f0e3b0effe5f1a736190753e7a7a54ea3fdc
pradip850 <2021WA86079@wilp.bits-pilani.ac.in>
2025-06-13 19:49:58 +0530

    Update and rename HelloWorld.java to src/HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java – V2");
-        System.out.println("Git versioning demo”);
+        System.out.println("Git versioning demo");
     }
 }
 
